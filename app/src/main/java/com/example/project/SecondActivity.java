e148f1d47fd06f491d298f0abe1d1123171e11eb
Gabriel Gomez Palomäki <a20gabpa@student.his.se>
2024-05-05 12:29:46 +0200

    Added ability to go back to main activity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,14 @@ public class SecondActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_second);
+
+        // Get button and add event listener
+        Button btn = findViewById(R.id.secondBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
