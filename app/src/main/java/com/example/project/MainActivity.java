c1c240af31d51c5e03a785b94ebbf1344665fa6a
Gabriel Gomez Palomäki <a20gabpa@student.his.se>
2024-05-05 12:26:35 +0200

    Added SecondActivity.java + Layout
    Implemented a second activity that also includes a different layout to the screen. Buttons have been added to each activity and only the button the main screen has been implemented.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,5 +33,16 @@ public class MainActivity extends AppCompatActivity {
         // Read from preferences
         TextView prefTextView = findViewById(R.id.prefText);
         prefTextView.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found..."));
+
+        // Get button and add event listener
+        Button btn = findViewById(R.id.firstBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
